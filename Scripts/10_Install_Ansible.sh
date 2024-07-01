de65dab79b47b889f5e6dcbf462feaa22567835e
James Radtke <cloudxabide@gmail.com>
2024-07-01 15:55:30 -0400

    generic update

---
+++ Scripts/10_Install_Ansible.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#     Purpose: Install Ansible
+#        Date:
+#      Status: Complete/Done
+# Assumptions:
+#        Todo:
+
+sudo apt-get install -y python-software-properties
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
+
+sudo apt-get install -y ansible
+ansible --version
+
+exit 0
+
