dd09ef786321e7910d46ec0a5359245d49c04fbd
James Radtke <cloudxabide@gmail.com>
2024-07-01 14:19:31 -0400

    generic update

--- Scripts/15_Install_BIND9.sh
+++ Scripts/15_Install_BIND9.sh
@@ -10,20 +10,21 @@ sudo apt install -y bind9 bind9utils bind9-doc
 sudo sed -i -e 's/bind/bind -4/g' /etc/default/named
 
 sudo cp /etc/bind/named.conf.options /etc/bind/named.conf.options.bak
-curl $REPO/main/Files/etc_bind_named.conf.options | sudo tee /etc/bind/named.conf.options
-sudo systemctl enable named.service --now
+curl ${REPO}main/Files/etc_bind_named.conf.options | sudo tee /etc/bind/named.conf.options
 
 # Add all of the zone files to the BIND config
 sudo cp /etc/bind/named.conf.local /etc/bind/named.conf.local.bak
-curl $REPO/main/Files/etc_bind_named.conf.local | sudo tee /etc/bind/named.conf.local
+curl ${REPO}main/Files/etc_bind_named.conf.local | sudo tee /etc/bind/named.conf.local
+
+sudo systemctl enable named.service --now
 
 sudo mkdir -p /etc/bind/zones
 for ZONE in 12 13 14 15
 do 
-  curl $REPO/main/Files/etc_bind_zones_db.$ZONE.10.10.in-addr.arpa | sudo tee /etc/bind/zones/db.$ZONE.10.10.in-addr.arpa
+  curl ${REPO}main/Files/etc_bind_zones_db.$ZONE.10.10.in-addr.arpa | sudo tee /etc/bind/zones/db.$ZONE.10.10.in-addr.arpa
 done 
-curl $REPO/main/Files/etc_bind_zones_db.kubernerdes.lab | sudo tee /etc/bind/zones/db.kubernerdes.lab
-curl $REPO/main/Files/etc_bind_zones_db.apps.kubernerdes.lab | sudo tee /etc/bind/zones/db.apps.kubernerdes.lab
+curl ${REPO}main/Files/etc_bind_zones_db.kubernerdes.lab | sudo tee /etc/bind/zones/db.kubernerdes.lab
+curl ${REPO}main/Files/etc_bind_zones_db.apps.kubernerdes.lab | sudo tee /etc/bind/zones/db.apps.kubernerdes.lab
 
 # Validate all the zone files
 cd /etc/bind/zones
@@ -32,17 +33,13 @@ for FILE in `ls *arpa`; do named-checkzone $(echo $FILE | sed 's/db.//g'; ) $FIL
 cd -
 
 # Restart Named Service
-systemctl restart bind9
+sudo systemctl restart bind9
 sudo systemctl restart named.service 
 
 # Reset the host lookups (hopefully)
 sudo systemctl restart systemd-resolved.service
-
-exit 0
-
 sudo resolvectl flush-caches
 
-references() {
-echo "https://www.digitalocean.com/community/tutorials/how-to-configure-bind-as-a-private-network-dns-server-on-ubuntu-22-04"
-echo "Who knows how old this is?  https://help.ubuntu.com/community/BIND9ServerHowto"
-}
+sudo systemctl status named.service 
+
+exit 0
