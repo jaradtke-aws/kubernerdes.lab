23b2816e54abcb6968ef13e73d8e6ab47f02afbe
James Radtke <cloudxabide@gmail.com>
2024-07-18 13:46:46 -0400

    generic update

--- Files/var_www_html_services.php
+++ Files/var_www_html_services.php
@@ -11,13 +11,16 @@
 <TR><TD><font color=blue>Namespace</TD><TD><font color=blue>Service</TD><TD><font color=blue>Endpoint</TD></TR>
 
 <?php
+$i=0;
 # You will need to copy the correct KUBECONFIG to the following location
 $kubeconfig="/var/www/html/kubernerdes-eksa-eks-a-cluster.kubeconfig";
 putenv ("KUBECONFIG=$kubeconfig");
 
+## A few different renditions of the syntax and formatting
 #$k_output = shell_exec('/usr/local/bin/kubectl get svc -A | grep LoadBalancer | awk \'{ split($6, ports, ":"); print $1 " | " $2 " | http://" $5":"ports[1] }\' ');
 #$k_output = shell_exec('/usr/local/bin/kubectl get svc -A | grep LoadBalancer | awk \'{ split($6, ports, ":"); print $1 " | " $2 " | <A HREF=http://" $5":" ports[1] ">" $5":" ports[1] "</A>" }\' ');
-$k_output = shell_exec('/usr/local/bin/kubectl get svc -A | grep LoadBalancer | awk \'{ split($6, ports, ":"); print "<TR><TD>" $1 "</TD> <TD>" $2 "</TD>  <TD><A HREF=http://" $5":" ports[1] ">" $5":" ports[1] "</A></TD></TR>" }\' ');
+#$k_output = shell_exec('/usr/local/bin/kubectl get svc -A | grep LoadBalancer | awk \'{ split($6, ports, ":"); print "<TR><TD>" $1 "</TD> <TD>" $2 "</TD>  <TD><A HREF=http://" $5":" ports[1] ">" $5":" ports[1] "</A></TD></TR>" }\' ');
+$k_output = shell_exec('/usr/local/bin/kubectl get svc -A | grep LoadBalancer | awk \'{ split($6, ports, ":"); print "<TR><TD>" $1 "</TD> <TD>" $2 "</TD>  <TD><A HREF=http://" $5":" ports[1] " target=\""$2"\" >" $5":" ports[1] "</A></TD></TR>" }\' ');
 
 echo "<pre>$k_output</pre>";
 
@@ -29,6 +32,9 @@ echo "<pre>$k_output</pre>";
 <TABLE>
 <TH colspan=3> Kubernerdes.Lab Infrastructure Services and Endpoints</TH>
 <TR><TD><font color=blue>Service</TD> <TD><font color=blue>Endpoint</TD></TR>
+<TR> <TD>vSphere Console</TD> <TD><A HREF="https://10.10.12.30/" target="vsphere">https://10.10.12.30/</A></TD> </TR>
+<TR> <TD>ESXi Console (vmw-esx-01)</TD> <TD><A HREF="http://10.10.12.31" target="esxi1">http://10.10.12.31</A></TD> </TR>
+
 <TR> <TD>vSphere Console</TD> <TD>https://10.10.12.30/</TD> </TR>
 <TR> <TD>ESXi Console (vmw-esx-01)</TD> <TD>http://10.10.12.31</TD> </TR>
 <TR> <TD></TD> <TD></TD> </TR>
